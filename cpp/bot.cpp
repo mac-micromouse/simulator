236ec972e69e96a4fb5b60a56204dc469857ae84
Liam Strilchuk <62297965+liamstrilchuk@users.noreply.github.com>
2026-07-29 13:53:01 -0400

    movement mechanics, basic wall following bot

--- cpp/bot.cpp
+++ cpp/bot.cpp
@@ -1,13 +1,105 @@
 #include "micromouse.h"
 
+// left motor pins
+const int IN1 = 16;
+const int IN2 = 17;
+const int ENA = 18;
+
+// right motor pins
+const int IN3 = 19;
+const int IN4 = 21;
+const int ENB = 22;
+
+// encoders
+const int ENC_L_A = 34;
+const int ENC_R_A = 35;
+
 VL53L1X frontToF(0), leftToF(1), rightToF(2);
 
 void setup() {
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(IN3, OUTPUT);
+	pinMode(IN4, OUTPUT);
+
+	ledcAttachPin(ENA, 0);
+	ledcAttachPin(ENB, 1);
+
+	pinMode(ENC_L_A, INPUT);
+	pinMode(ENC_R_A, INPUT);
+
+	frontToF.init();
+	leftToF.init();
+	rightToF.init();
+}
+
+void driveForward(int speed) {
+	digitalWrite(IN1, HIGH);
+	digitalWrite(IN2, LOW);
+	digitalWrite(IN3, HIGH);
+	digitalWrite(IN4, LOW);
+
+	ledcWrite(0, speed);
+	ledcWrite(1, speed);
+}
+
+void turnRight(int speed) {
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, HIGH);
+	digitalWrite(IN3, HIGH);
+	digitalWrite(IN4, LOW);
+
+	ledcWrite(0, speed);
+	ledcWrite(1, speed);
+}
+
+void turnLeft(int speed) {
+	digitalWrite(IN1, HIGH);
+	digitalWrite(IN2, LOW);
+	digitalWrite(IN3, LOW);
+	digitalWrite(IN4, HIGH);
+
+	ledcWrite(0, speed);
+	ledcWrite(1, speed);
+}
+
+void stop() {
+	driveForward(0);
 }
 
 void loop() {
-	Serial.println(frontToF.read());
-	delay(20);
+	uint16_t frontDist = frontToF.read();
+	uint16_t leftDist = leftToF.read();
+	uint16_t rightDist = rightToF.read();
+
+	if (frontDist > 150) {
+		driveForward(255);
+	} else if (rightDist > 100) {
+		turnRight(255);
+		delay(1000);
+		stop();
+	} else if (leftDist > 100) {
+		turnLeft(255);
+		delay(1000);
+		stop();
+	} else {
+		while (frontDist <= 100) {
+			turnRight(255);
+			delay(1000);
+			stop();
+			frontDist = frontToF.read();
+		}
+	}
+
+	Serial.print(frontDist);
+	Serial.print(" ");
+	Serial.print(leftDist);
+	Serial.print(" ");
+	Serial.print(rightDist);
+	Serial.println();
+	delay(335);
+	stop();
+	delay(100);
 }
 
 int main() {
--- js/Bot.js
+++ js/Bot.js
@@ -1,3 +1,7 @@
+const DIST_BETWEEN_WHEELS = 70;
+const WHEEL_RADIUS = 15;
+const MAX_SPEED = 15;
+
 class Bot {
 	constructor(x, y) {
 		this.x = x;
@@ -13,6 +17,33 @@ class Bot {
 	}
 
 	update() {
+		const leftDir = this.getPin(16) - this.getPin(17);
+		const rightDir = this.getPin(19) - this.getPin(21);
+
+		const leftPWM = this.getPWM(0) / 255;
+		const rightPWM = this.getPWM(1) / 255;
+
+		const velLeft = leftDir * leftPWM * MAX_SPEED;
+		const velRight = rightDir * rightPWM * MAX_SPEED;
+
+		const dt = 60 / 1000;
+
+		const distL = velLeft * dt;
+		const distR = velRight * dt;
+		const centerDist = (distL + distR) / 2;
+		const deltaTheta = (distR - distL) / DIST_BETWEEN_WHEELS;
+
+		this.rotation += deltaTheta;
+		this.x += centerDist * Math.cos(this.rotation);
+		this.y += centerDist * Math.sin(this.rotation);
+	}
+
+	getPin(pinNumber) {
+		return this.pins[pinNumber] || 0;
+	}
+
+	getPWM(channel) {
+		return this.pwm[channel] || 0;
 	}
 
 	render(ctx) {
