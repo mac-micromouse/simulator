045effcac18218a3560eb09bcf4418685f004259
Liam Strilchuk <62297965+liamstrilchuk@users.noreply.github.com>
2026-07-31 23:01:42 -0400

    save editor value; save indicator

--- js/interface/EditorInterface.js
+++ js/interface/EditorInterface.js
@@ -6,5 +6,26 @@ class EditorInterface {
 		this.editor.container.style.lineHeight = 1.4;
 		this.editor.renderer.updateFontSize();
 		this.editor.session.setMode("ace/mode/c_cpp");
+
+		if (localStorage.getItem("editor-value")) {
+			this.editor.setValue(localStorage.getItem("editor-value"), -1);
+		}
+
+		const saveButton = document.getElementById("save-button");
+
+		this.saveTimeout = null;
+		this.editor.session.on("change", () => {
+			saveButton.children[0].classList.replace("fa-check", "fa-ellipsis");
+			saveButton.title = "Not saved...";
+			saveButton.style.color = "rgb(204, 208, 214)";
+			window.clearTimeout(this.saveTimeout);
+
+			this.saveTimeout = window.setTimeout(() => {
+				localStorage.setItem("editor-value", this.editor.getValue());
+				saveButton.children[0].classList.replace("fa-ellipsis", "fa-check");
+				saveButton.title = "Saved!";
+				saveButton.style.color = "rgb(100, 230, 100)";
+			}, 3000);
+		});
 	}
 }
