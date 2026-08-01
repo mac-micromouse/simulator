d4ae54ba676682a6fd2c6e49e2911285b48f234d
Liam Strilchuk <62297965+liamstrilchuk@users.noreply.github.com>
2026-08-01 10:09:57 -0400

    switch between tabs

--- js/interface/EditorInterface.js
+++ js/interface/EditorInterface.js
@@ -16,7 +16,7 @@ class EditorInterface {
 		this.saveTimeout = null;
 		this.editor.session.on("change", () => {
 			saveButton.children[0].classList.replace("fa-check", "fa-ellipsis");
-			saveButton.title = "Not saved...";
+			saveButton.title = "Saving...";
 			saveButton.style.color = "rgb(204, 208, 214)";
 			window.clearTimeout(this.saveTimeout);
 
@@ -27,5 +27,27 @@ class EditorInterface {
 				saveButton.style.color = "rgb(100, 230, 100)";
 			}, 3000);
 		});
+
+		const tabEditor = document.getElementById("tab-editor");
+		const tabDocumentation = document.getElementById("tab-documentation");
+		const tabExamples = document.getElementById("tab-examples");
+
+		const editorContainer = document.getElementById("editor-container");
+		const documentationContainer = document.getElementById("documentation-container");
+		const examplesContainer = document.getElementById("examples-container");
+
+		tabEditor.addEventListener("click", () => this.selectLeftTab(editorContainer, tabEditor));
+		tabDocumentation.addEventListener("click", () => this.selectLeftTab(documentationContainer, tabDocumentation));
+		tabExamples.addEventListener("click", () => this.selectLeftTab(examplesContainer, tabExamples));
+	}
+
+	selectLeftTab(tab, button) {
+		[...document.querySelectorAll(".left-tab")].forEach(item => item.style.display = "none");
+		tab.style.display = "block";
+
+		[...document.getElementById("container-left").querySelectorAll(".selected")]
+			.forEach(elem => elem.classList.remove("selected"));
+
+		button.classList.add("selected");
 	}
 }
